9a728d18bb93d04450e7f5839cf035610698618a
viviweikuo <mei200709@gmail.com>
2022-12-18 20:43:16 +0800

    Assignment: home page, product page & membership

---
+++ public/scripts-attraction.js
@@ -0,0 +1,133 @@
+
+let attractionPath = location.pathname;
+let attractionId = attractionPath.replace("/attraction/", "");
+
+function fetchAttractionData(){
+
+    fetch("http://18.213.194.28:3000/api/attraction/"+attractionId)
+    .then((response) => {
+        return response.json();
+    })
+    .then((attraction) => {
+
+        for(let a in attraction){
+
+            // 2. name
+            let attractionName = document.querySelector(".product-booking-title");
+            attractionName.textContent = attraction.data.name;
+
+            // 3. category + "at" + mrt
+            let attractionDirection = document.querySelector(".product-booking-direction");
+
+            let attractionDirectionCategory = document.createElement("span");
+            attractionDirectionCategory.classList.add("product-booking-direction-category");
+            attractionDirectionCategory.textContent = attraction.data.category;
+
+            let attractionDirectionMrt = document.createElement("span");
+            attractionDirectionMrt.classList.add("product-booking-direction-mrt");
+            attractionDirectionMrt.textContent = attraction.data.mrt;
+
+            let attractionDirectionAt = document.querySelector(".product-booking-direction-at");
+            attractionDirection.insertBefore(attractionDirectionCategory, attractionDirectionAt);
+            attractionDirection.appendChild(attractionDirectionMrt);
+
+            // 4. description
+            let attractionArticle = document.querySelector(".product-introduction-article-content");
+            attractionArticle.textContent = attraction.data.description;
+
+            // 5. address
+            let attractionAddress = document.querySelector(".product-introduction-address-content");
+            attractionAddress.textContent = attraction.data.address;
+
+            // 6. transport
+            let attractionTransport = document.querySelector(".product-introduction-transport-content");
+            attractionTransport.textContent = attraction.data.transport;
+
+            // 1. images
+            let attractionImageBox = document.querySelector(".images-container-box");
+
+            for (let i in attraction.data.images){
+                let attractionImageBoxSub = document.createElement("div");
+                attractionImageBoxSub.classList.add("attraction-image-box");
+                attractionImageBoxSub.classList.add("slide");
+                attractionImageBoxSub.classList.add("fade");
+
+                let attractionImage = document.createElement("img");
+                attractionImage.classList.add("attraction-image");
+                attractionImage.style.width = "100%";
+                attractionImage.src = attraction.data.images[i];
+
+                attractionImageBoxSub.appendChild(attractionImage);
+                attractionImageBox.appendChild(attractionImageBoxSub);
+
+                let attractionImageDotBox = document.querySelector(".dots-box");
+                attractionImageDotBox.innerHTML += "<span class='dot' onclick='currentSlide(n)'></span>";
+
+                showSlide(slideIndex);
+            }
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
+fetchAttractionData();
+
+function checkPeriod(){
+    
+    let morningSchedule = document.querySelector(".product-booking-period-morning-btn-sub");
+    let afternoonSchedule = document.querySelector(".product-booking-period-afternoon-btn-sub");
+    let morningSchedulePrice = document.querySelector(".product-price-morning");
+    let afternoonSchedulePrice = document.querySelector(".product-price-afternoon");
+
+    morningSchedule.classList.toggle("btn-on");
+    morningSchedule.classList.toggle("btn-off");
+
+    afternoonSchedule.classList.toggle("btn-off");
+    afternoonSchedule.classList.toggle("btn-on");
+
+    morningSchedulePrice.classList.toggle("hide-text");
+    afternoonSchedulePrice.classList.toggle("hide-text");
+
+}
+checkPeriod();
+
+let slideIndex = 1;
+
+function plusSlide(n){
+    showSlide(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n){
+
+    let slides = document.getElementsByClassName("slide");
+
+    if (n > slides.length){ slideIndex = 1; }
+    if (n < 1){ slideIndex = slides.length; }
+
+    for (let i = 0 ; i < slides.length ; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dotsPlay();
+}
+
+function dotsPlay(){
+    
+    let dots = document.getElementsByClassName("dot");
+    console.log(dots);
+
+    let dots_array = Array.from(dots);
+    console.log(dots_array);
+
+    for (let j = 0 ; j < dots_array.length ; j++){
+        dots_array[j].className = dots_array[j].className.replace(" dot-active", "");
+    }
+
+    dots_array[slideIndex-1].className += " dot-active";
+}
---
+++ public/scripts-membership.js
@@ -0,0 +1,254 @@
+// sign-in
+function sendSigninData(){
+    let userEmail = document.querySelector(".login-email-input").value;
+    let userPassword = document.querySelector(".login-password-input").value;
+    let userData = {
+        "email": userEmail,
+        "password": userPassword
+        };
+    const request = new XMLHttpRequest();
+    request.open("PUT", "/api/user/auth");
+    request.send(userData);
+    memberSignin();
+}
+
+function memberSignin(){
+    let errorMessage = document.querySelector(".login-error-message");
+
+    let userEmail = document.querySelector(".login-email-input").value;
+    let userPassword = document.querySelector(".login-password-input").value;
+    let userData = {
+        "email": userEmail,
+        "password": userPassword
+        };
+
+    fetch("http://18.213.194.28:3000/api/user/auth", {
+        method: "PUT",
+        body: JSON.stringify(userData),
+        credentials: "include",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json",
+        }
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+
+            if (result["ok"]){
+                location.reload();
+            } else {
+                errorMessage.innerHTML = result["message"];
+                errorMessage.classList.remove("hide-content");
+                errorMessage.classList.add("open-content");
+                errorMessage.classList.add("error-message");
+
+                let signinPopForm = document.querySelector(".login-popup");
+                signinPopForm.style.height = "300px";
+
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+// sign-up
+function sendSignupData(){
+    let userName = document.querySelector(".signup-name-input").value;
+    let userEmail = document.querySelector(".signup-email-input").value;
+    let userPassword = document.querySelector(".signup-password-input").value;
+    let userData = {
+        "name": userName,
+        "email": userEmail,
+        "password": userPassword
+        };
+    const request = new XMLHttpRequest();
+    request.open("POST", "/api/user");
+    request.send(userData);
+    memberSignup();
+}
+
+function memberSignup(){
+    let errorMessage = document.querySelector(".signup-error-message")
+
+    let userName = document.querySelector(".signup-name-input").value;
+    let userEmail = document.querySelector(".signup-email-input").value;
+    let userPassword = document.querySelector(".signup-password-input").value;
+    let userData = {
+        "name": userName,
+        "email": userEmail,
+        "password": userPassword
+        };
+    
+    fetch("http://18.213.194.28:3000/api/user", {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json",
+        }
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+
+            if (result["ok"]){
+                errorMessage.innerHTML = "註冊完成";
+                errorMessage.classList.remove("hide-content");
+                errorMessage.classList.add("open-content");
+                errorMessage.classList.add("error-message");
+
+                let signupPopForm = document.querySelector(".signup-popup");
+                signupPopForm.style.height = "345px";
+
+            } else {
+                errorMessage.innerHTML = result["message"];
+                errorMessage.classList.remove("hide-content");
+                errorMessage.classList.add("open-content");
+                errorMessage.classList.add("error-message");
+
+                let signupPopForm = document.querySelector(".signup-popup");
+                signupPopForm.style.height = "345px";
+
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+// check login status
+function checkLogin(){
+
+    fetch("http://18.213.194.28:3000/api/user/auth", {
+        method: "GET",
+        credentials: "include",
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+
+            if (result.data != null){
+                let signinButton = document.querySelector(".signin-signup-btn");
+                signinButton.classList.add("hide-content");
+                let logoutButton = document.querySelector(".logout-btn");
+                logoutButton.style.display = "block";
+                logoutButton.classList.remove("hide-content");
+                logoutButton.classList.add("open-content");
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+checkLogin();
+
+// log-out
+function logout(){
+
+    fetch("http://18.213.194.28:3000/api/user/auth", {
+        method: "DELETE",
+        credentials: "include",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json",
+        }
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+
+            if (result["ok"]){
+                location.reload();
+                let signinButton = document.querySelector(".signin-signup-btn");
+                signinButton.classList.remove("hide-content");
+                let logoutButton = document.querySelector(".logout-btn");
+                logoutButton.classList.remove("open-content");
+                logoutButton.classList.add("hide-content");
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
+
+// close-form
+function toggleForm(){
+    let membershipForm = document.querySelector(".membership-container"); 
+    membershipForm.classList.add("hide-content");
+    let backgroundGray = document.querySelector(".background-layer");
+    backgroundGray.classList.add("hide-content");
+
+    // click signin/signup btn
+    let loginButton = document.querySelector(".signin-signup-btn");
+
+    loginButton.addEventListener("click", () => {
+        membershipForm.classList.remove("hide-content");
+        membershipForm.classList.add("open-content");
+
+        backgroundGray.classList.remove("hide-content");
+        backgroundGray.classList.add("open-content");
+    })
+
+    // click close btn
+    let closeButton = document.querySelector(".close-btn");
+
+    closeButton.addEventListener("click", () => {
+        membershipForm.classList.remove("open-content");
+        membershipForm.classList.add("hide-content");
+
+        backgroundGray.classList.remove("open-content");
+        backgroundGray.classList.add("hide-content");
+    }, true);
+}
+toggleForm();
+
+// change-form
+function changeForm(){
+    let signinForm = document.querySelector(".login-popup");
+    signinForm.classList.add("open-content");
+    let signupForm = document.querySelector(".signup-popup");
+    signupForm.classList.add("hide-content");
+
+    // change form signin => signup
+    let changeToSignup = document.querySelector(".change-signin-to-signup");
+
+    changeToSignup.addEventListener("click", () => {
+        signupForm.classList.add("open-content");
+        signupForm.classList.remove("hide-content");
+        signinForm.classList.add("hide-content");
+        signinForm.classList.remove("open-content");
+    });
+
+    // change form signup => signin
+    let changeToSignin = document.querySelector(".change-signup-to-signin");
+
+    changeToSignin.addEventListener("click", () => {
+        signinForm.classList.remove("hide-content");
+        signinForm.classList.add("open-content");
+        signupForm.classList.remove("open-content");
+        signupForm.classList.add("hide-content");
+    });
+
+    styleForm();
+}
+changeForm();
+
+function styleForm(){
+    let loginErrorMessage = document.querySelector(".login-error-message");
+    loginErrorMessage.classList.remove("open-content");
+    loginErrorMessage.classList.add("hide-content");
+    let signinPopForm = document.querySelector(".login-popup");
+    signinPopForm.style.height = "275px";
+
+    let signupErrorMessage = document.querySelector(".signup-error-message");
+    signupErrorMessage.classList.remove("open-content");
+    signupErrorMessage.classList.add("hide-content");
+    let signupPopForm = document.querySelector(".signup-popup");
+    signupPopForm.style.height = "330px";
+}
